923c316c43b7e5141c3784f11625d8180042a5ed
Ricardo Jesus <rdo_ramone@hotmail.com>
2020-09-29 20:23:20 -0300

    Start do repositório para testes com Vuex.
    Aplicação da store na aplicação com um simples contador para entendimento do funcionamento.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Count />
+  </div>
+</template>
+
+<script>
+import Count from './components/Count.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Count
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Count.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <p>{{ count }}</p>
+  <div>
+    <button @click="increment()">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</div>
+</template>
+  
+<script>
+import store from '../store/index.js'
+
+export default {
+  name: 'Count',
+  computed: {
+    count() {
+      return store.state.count
+    } 
+  },
+  methods: {
+    increment() {
+      store.commit('increment')
+    },
+    decrement() {
+      store.commit('decrement')
+    }
+  }
+}
+</script>
