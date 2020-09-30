67b7fe7686081b0310ebab703c966b6ef5e7ac74
Ricardo Jesus <rdo_ramone@hotmail.com>
2020-09-30 09:04:20 -0300

    Mapeamento de getters.

--- src/components/Getters.vue
+++ src/components/Getters.vue
@@ -8,17 +8,38 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   computed: {
-    doneTodos() {
-      return this.$store.getters.doneTodos;
-    },
-    totalDoneTodos() {
-      return this.$store.getters.totalDoneTodos;
-    },
+    // doneTodos() {
+    //   return this.$store.getters.doneTodos;
+    // },
+    // totalDoneTodos() {
+    //   return this.$store.getters.totalDoneTodos;
+    // },
     getTodoById() {
       return this.$store.getters.getTodoById(2);
-    }
+    },
+    /*
+      Aqui estou usando o auxiliar mapGetters para mapear 2 getters que estão na Store,
+      basta passar o nome dados a eles dentro da Store através de uma string e usá-los
+      com o mesmo nome dentro do template.
+    */
+    ...mapGetters([
+      'doneTodos',
+      'totalDoneTodos'
+    ])
+
+    /*
+      Aqui eu posso nomear o meu getter que está sendo mapeado, sendo assim, basta
+      passar um objeto para o mapGetters e dentro dele a propriedade com o nome
+      desejado e passar o valor do getter que está na store.
+
+      ...mapGetters({
+        totalDone: 'totalDoneTodos'
+      })
+    */
   }
 }
 </script>
